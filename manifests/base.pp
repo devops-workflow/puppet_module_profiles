4a0501971edfd67f136cf5c1df56f12f5520dc26
Steven Nemetz <steven.nemetz@fireeye.com>
2015-08-12 14:12:21 -0700

    Add epel repo to base

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,6 +4,7 @@
 # Install and configure base OS and utilities
 #
 class profiles::base {
+  include epel
   $pkg_utils = ['curl', 'sysstat', 'tcpdump', 'telnet', 'traceroute', 'unzip', 'wget', 'xdelta' ]
   package { $pkg_utils: ensure => latest, }
 }
