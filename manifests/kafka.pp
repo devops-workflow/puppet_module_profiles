5a17c94009a013cded2367e09742149caf5347bf
Steven Nemetz <steven.nemetz@fireeye.com>
2015-08-13 15:06:11 -0700

    Configuring kafka server

--- manifests/kafka.pp
+++ manifests/kafka.pp
@@ -4,30 +4,38 @@
 # Install and configure 
 #
 class profiles::kafka {
-  class { '::kafka':
+#  class { '::kafka':
+#    version       => '0.8.2.1',
+#    scala_version => '2.10',
+#    #install_dir   => 
+#    #mirror_url    => 
+#  }
+
+  class { '::kafka::broker':
     version       => '0.8.2.1',
     scala_version => '2.10',
 #    install_dir   => 
 #    mirror_url    => 
     config        => {
-      'port'                            => 25700,
+      'broker.id'                       => '0',
+      'port'                            => '25700',
       'host.name'                       => '192.168.30.20',
-      'num.network.threads'             => 2,
-      'socket.send.buffer.bytes'        => 1048576,
-      'socket.receive.buffer.bytes'     => 1048576,
+      'num.network.threads'             => '2',
+      'socket.send.buffer.bytes'        => '1048576',
+      'socket.receive.buffer.bytes'     => '1048576',
+      'socket.request.max.bytes'        => '104857600',
       'log.dirs'                        => '/var/log/kafka',
-      'auto.create.topics.enable'       => false,
-      'num.partitions'                  => 2,
-      'log.flush.interval.messages'     => 10000,
-      'log.flush.interval.ms'           => 1000,
-      'log.retention.hours'             => 24,
-      'log.segment.bytes'               => 536870912,
-      'log.cleanup.interval.mins'       => 1,
+      'auto.create.topics.enable'       => 'false',
+      'num.partitions'                  => '2',
+      'log.flush.interval.messages'     => '10000',
+      'log.flush.interval.ms'           => '1000',
+      'log.retention.hours'             => '24',
+      'log.segment.bytes'               => '536870912',
+      'log.cleanup.interval.mins'       => '1',
       'zookeeper.connect'               => '192.168.30.20:25699',
-      'zookeeper.connection.timeout.ms' => 1000000,
+      'zookeeper.connection.timeout.ms' => '1000000',
     }
   }
-#  class { '::kafka::broker': }
 #  class { '::kafka::consumer': }
 #  class { '::kafka::producer': }
 }
