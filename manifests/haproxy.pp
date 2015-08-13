53fb69ecbc3bf47e0b05015314f80d0a71b02284
Steven Nemetz <steven.nemetz@fireeye.com>
2015-08-13 13:05:27 -0700

    Update config

--- manifests/haproxy.pp
+++ manifests/haproxy.pp
@@ -8,18 +8,22 @@ class profiles::haproxy {
   # Move data/parameters to Hiera once this is working
   class { '::haproxy':
     global_options  => {
-      'log'     => "${::ipaddress} local0",
-      #'chroot'  => '/var/lib/haproxy',
-      'pidfile' => '/var/run/haproxy.pid',
-      #'maxconn' => '4000',
-      'user'    => 'haproxy',
-      'group'   => 'haproxy',
-      'daemon'  => '',
-      'stats'   => 'socket /var/lib/haproxy/stats',
-      # ssl, ...
+      'log'                      => "${::ipaddress} local0",
+      #'chroot'                   => '/var/lib/haproxy',
+      'pidfile'                  => '/var/run/haproxy.pid',
+      #'maxconn'                  => '4000',
+      'user'                     => 'haproxy',
+      'group'                    => 'haproxy',
+      'daemon'                   => '',
+      'stats'                    => 'socket /var/lib/haproxy/stats',
+      'ca-base'                  => '/etc/ssl/certs',
+      'crt-base'                 => '/etc/ssl/private',
+      'ssl-default-bind-ciphers' => 'kEECDH+aRSA+AES:kRSA+AES:+AES256:RC4-SHA:!kEDH:!LOW:!EXP:!MD5:!aNULL:!eNULL',
+      'ssl-default-bind-options' => 'no-sslv3',
     },
     defaults_options => {
       'log'     => 'global',
+      'mode'    => 'http',
       'stats'   => 'enable',
       'option'  => [
         'httplog',
@@ -29,9 +33,9 @@ class profiles::haproxy {
       'timeout' => [
         'http-request 10s',
         'queue 1m',
-        'connect 10s',
-        'client 1m',
-        'server 1m',
+        'connect 60s',
+        'client 60s',
+        'server 60s',
         'check 10s',
       ],
       'maxconn' => '8000',
@@ -98,10 +102,9 @@ class profiles::haproxy {
     #ipaddress     => $::ipaddress,
     #ports         => '443',
     #mode          => 'http',
-    options       => [
-      { 'default_backend' => 'httpsBackEnd' },
-    ],
-    bind      => {
+    options       => { 'default_backend' => 'httpsBackEnd' },
+    # ],
+    bind          => {
       "${::ipaddress}:443" => ['ssl', 'crt', '/etc/haproxy/dummy.pem'],
     }
   }
@@ -117,12 +120,14 @@ class profiles::haproxy {
   }
 
   haproxy::backend { 'httpsBackEnd':
-    options => {
+    mode          => 'http',
+    options       => {
       'option'  => [
         'httpclose',
         'forwardfor',
       ],
       'balance' => 'roundrobin',
+      'mode'    => 'http',
     },
   }
 
