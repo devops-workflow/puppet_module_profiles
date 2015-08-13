5612eb6a829f7dc70c5b9ab922af682503a4460b
Steven Nemetz <steven.nemetz@fireeye.com>
2015-08-13 15:19:43 -0700

    Initial config done

--- manifests/kafka.pp
+++ manifests/kafka.pp
@@ -39,25 +39,3 @@ class profiles::kafka {
 #  class { '::kafka::consumer': }
 #  class { '::kafka::producer': }
 }
-
-# Build this config:
-# #broker.id=0
-#port=25700
-#host.name=192.168.30.20
-#num.network.threads=2
-#num.io.threads=2
-#socket.send.buffer.bytes=1048576
-#socket.receive.buffer.bytes=1048576
-# #socket.request.max.bytes=104857600
-#log.dirs=/var/log/kafka
-#auto.create.topics.enable=false
-#num.partitions=2
-#log.flush.interval.messages=10000
-#log.flush.interval.ms=1000
-#log.retention.hours=24
-#log.segment.bytes=536870912
-#log.cleanup.interval.mins=1
-#
-#zookeeper.connect=192.168.30.20:25699
-#zookeeper.connection.timeout.ms=1000000
-
