d3e3e5e179b1cc2dcccad1eba52230591158c1ae
Steven Nemetz <steven.nemetz@fireeye.com>
2015-08-14 08:00:41 -0700

    Start configuring zookeeper

--- manifests/zookeeper.pp
+++ manifests/zookeeper.pp
@@ -4,4 +4,53 @@
 # Install and configure 
 #
 class profiles::zookeeper {
+  class { '::zookeeper':
+    id                     => 1,
+    url                    => 'http://www.eu.apache.org/dist/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz',
+    digest_string          => '971c379ba65714fd25dc5fe8f14e9ad1',
+    follow_redirects       => true,
+    extension              => 'tar.gz',
+    digest_type            => 'md5',
+    user                   => 'zookeeper',
+    manage_user            => true,
+    tmpDir                 => '/tmp',
+    installDir             => '/opt',
+    jvmFlags               => '-Dzookeeper.log.threshold=INFO -Xmx1g',
+    checksum               => true,
+    clientPortAddress      => "127.0.0.1",
+    cnxTimeout             => 20000,
+    configDir              => '/etc/zookeeper',
+    create_aio_service     => true,
+    dataDir                => '/var/lib/zookeeper',
+    # Move to separate drive
+    dataLogDir             => '/var/log/zookeeper',
+    globalOutstandingLimit => 1000,
+    maxClientCnxns         => 60,
+    snapCount              => 100000,
+    purgeInterval          => 1,
+    snapRetainCount        => 3,
+    tickTime               => 2000,
+    leaderServes           => 'yes',
+    servers                => {
+      0 => {
+           ip           => '192.168.30.20',
+           leaderPort   => 2888,
+           electionPort => 3888,
+      }
+    },
+    standaloneEnabled      => true,
+    syncEnabled            => true,
+    syncLimit              => 5,
+    electionAlg            => 3,
+    initLimit              => 10,
+    clientPort             => 25699,
+    leaderPort             => 3888,
+    leaderElectionPort     => 2888,
+    install_java           => true,
+    java_package           => 'java-1.8.0-openjdk',
+    manage_firewall        => true,
+    manage_service         => true,
+    service_name           => 'zookeeper', 
+  }
+  # zookeeper::resource::configuration {'localhost': }
 }
