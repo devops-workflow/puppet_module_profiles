37d272aed52d53088ce5c3b56ccea830949d2ba7
Steven Nemetz <steven.nemetz@fireeye.com>
2015-09-22 15:24:52 -0700

    Add puppet-lint plugins

--- manifests/jenkins/slave.pp
+++ manifests/jenkins/slave.pp
@@ -6,11 +6,25 @@
 class profiles::jenkins::slave {
   include epel
 
+  #
+  # Support for Puppet
+  #
   package { 'puppet-lint':
     ensure   => latest,
     provider => 'pe_gem',
   }
   # Add puppet-lint plugins
+  $puppet_lint_plugins = [
+    'puppet-lint-absolute_template_path',
+    'puppet-lint-param-docs', 'puppet-lint-roles_and_profiles-check',
+    'puppet-lint-strict_indent-check', 'puppet-lint-trailing_newlines-check',
+    'puppet-lint-unquoted_string-check', 'puppet-lint-variable_contains_upcase'
+  ]
+  package { $puppet_lint_plugins :
+    ensure   => latest,
+    provider => 'pe_gem',
+  }
+
   package { 'lsb': ensure => latest, }
   package { 'jq': ensure => latest, }
 }
