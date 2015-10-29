28cbc3a6d4f66a68058697102e52bc808e4f41fe
Mike Nolte <obiwanmikenolte@gmail.com>
2015-10-29 13:40:48 -0600

    Fixing typos in firewall.pp; Adding firewall rules to Puppet master

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -34,20 +34,20 @@ class profiles::firewall {
     outiface => 'lo',
     action   => 'accept',
   } ->
-  firewall { '302 Reject outound, local traffic not on loopback interface':
+  firewall { '302 Reject outbound, local traffic not on loopback interface':
     chain       => 'OUTPUT',
     outiface    => '! lo',
     proto       => 'all',
     destination => '127.0.0.1/8',
     action      => 'reject',
   } ->
-  firewall { '303 Accept inbound RELATED, ESTABLISHED traffic':
+  firewall { '303 Accept outbound RELATED, ESTABLISHED traffic':
     chain  => 'OUTPUT',
     proto  => 'all',
     state  => ['RELATED', 'ESTABLISHED'],
     action => 'accept',
   } ->
-  firewall { '304 Accept inbound SSH':
+  firewall { '304 Accept outbound SSH':
     chain  => 'OUTPUT',
     dport  => '22',
     proto  => tcp,
--- manifests/puppet/master.pp
+++ manifests/puppet/master.pp
@@ -1,7 +1,19 @@
 class profiles::puppet::master {
+
   file { '/etc/hiera.yaml':
     ensure  => link,
     target  => '/etc/puppet/hiera.yaml',
     require => Class['::puppet'],
   }
+
+  firewall { '100 Accept inbound Puppet traffic':
+    proto  => '8140',
+    action => 'accept',
+  }
+  firewall { '100 Accept outbound Puppet traffic':
+    chain  => 'OUTPUT',
+    proto  => '443',
+    action => 'accept',
+  }
+
 }
