cdbbbaa02b8d6a99dbed2118f8dda0aa62e5de5c
Russell M. Van Tassell <russell@geekoncall.net>
2015-11-03 12:37:38 -0800

    Better placeholder profile for nsswitch

--- manifests/nsswitch.pp
+++ manifests/nsswitch.pp
@@ -30,48 +30,64 @@ class profiles::nsswitch {
   #
   # This should likely be moved to Hiera...
   #
-  class ::nsswitch {
-    case $::osfamily {
-      'Debian': {
-        $passwd     = ['compat']
-        $group      = ['compat']
-        $shadow     = ['compat']
-        $hosts      = ['files','myhostname','dns']
-        #$bootparams = []
-        #$aliases    = []
-        #automount  = []
-        $ethers     = ['db','files']
-        $netgroup   = ['nis']
-        #$netmasks   = []
-        # $networks?
-        #$network    = []
-        $protocols  = ['db','files']
-        #$publickey  = []
-        $rpc        = ['db','files']
-        $services   = ['db','files']
-        #$sudo       = []
-      }
-      'RedHat': {
-        $passwd     = ['files', 'sss']
-        $group      = ['files', 'sss']
-        $shadow     = ['files', 'sss']
-        $hosts      = ['files', 'dns']
-        $bootparams = ['nisplus', '[NOTFOUND=return]', 'files' ]
-        $aliases    = ['files','nisplus']
-        $automount  = ['files']
-        $ethers     = ['files']
-        $netgroup   = ['files', 'sss']
-        $netmasks   = ['files']
-        # $networks?
-        $network    = ['files']
-        $protocols  = ['files']
-        $publickey  = ['nisplus']
-        $rpc        = ['files']
-        $services   = ['files', 'sss']
-        #$sudo       = ['files']
-      }
-      default: { warning( "Operating System not defined for $::modulename" ) }
+  case $::osfamily {
+    'Debian': {
+      $passwd     = ['compat']
+      $group      = ['compat']
+      $shadow     = ['compat']
+      $hosts      = ['files','myhostname','dns']
+      #$bootparams = []
+      #$aliases    = []
+      #automount  = []
+      $ethers     = ['db','files']
+      $netgroup   = ['nis']
+      #$netmasks   = []
+      $networks   = []
+      $protocols  = ['db','files']
+      #$publickey  = []
+      $rpc        = ['db','files']
+      $services   = ['db','files']
+      #$sudo       = []
     }
+    'RedHat': {
+      $passwd     = ['files', 'sss']
+      $group      = ['files', 'sss']
+      $shadow     = ['files', 'sss']
+      $hosts      = ['files', 'dns']
+      $bootparams = ['nisplus', '[NOTFOUND=return]', 'files' ]
+      $aliases    = ['files','nisplus']
+      $automount  = ['files']
+      $ethers     = ['files']
+      $netgroup   = ['files', 'sss']
+      $netmasks   = ['files']
+      $networks   = ['files']
+      $protocols  = ['files']
+      $publickey  = ['nisplus']
+      $rpc        = ['files']
+      $services   = ['files', 'sss']
+      #$sudo       = ['files']
+    }
+    default: { warning( "Operating System not defined for $::modulename" ) }
+  }
+
+  class ::nsswitch {
+    $passwd     => $passwd,
+    $group      => $group,
+    $shadow     => $shadow,
+    $hosts      => $hosts,
+    $bootparams => $bootparams,
+    $aliases    => $aliases,
+    $automount  => $automount,
+    $ethers     => $ethers,
+    $netgroup   => $netgroup,
+    $netmasks   => $netmasks,
+    # $networks?
+    $network    => $networks,
+    $protocols  => $protocols,
+    $publickey  => $publickey,
+    $rpc        => $rpc,
+    $services   => $services,
+    #$sudo       => $sudo,
   }
 }
 
