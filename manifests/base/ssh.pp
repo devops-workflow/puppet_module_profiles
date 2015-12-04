4f716ce6cc8ac3917218d8410e1fb56c5278ba13
Russell M. Van Tassell <russell@geekoncall.net>
2015-12-04 15:28:30 -0800

    Adds ssh to base.pp

---
+++ manifests/base/ssh.pp
@@ -0,0 +1,10 @@
+# == Class: profiles::ssh
+# ================================
+#
+# Install and configure of saz/ssh
+#
+# Ref: https://forge.puppetlabs.com/saz/ssh
+#
+class profiles::base::ssh {
+  include ::ssh
+}
